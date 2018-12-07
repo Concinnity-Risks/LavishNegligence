3fa4447871d425bfcba8e5f1ac91d5a2b8ffcfbb
blackswanburst <eireann.leverett@cantab.net>
2018-12-07 17:26:13 +0000

    Changed the bash script

--- TLS-Sampler.sh
+++ TLS-Sampler.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sleep $(($RANDOM % 21600 )); cd /home/praxis/LavishNegligence && python CA-Cert-Graph.py >> TLS-Error.log
+sleep $(($RANDOM % 21600 )); cd /home/praxis/LavishNegligence && python CertificateFetcher.py >> TLS-Error.log
